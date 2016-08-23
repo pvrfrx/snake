f66f1fdaaab6ce3c73b01395129eb114799196d2
pvrfrx@gmail.com <javarush1>
2016-08-23 14:34:55 +0500

    add listChar and add listPoint

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -18,7 +18,27 @@ namespace snake
             p2.Draw();
 
             Console.ReadLine();
+            Console.WriteLine("Шестой урок");
+
+            List<char> listChar = new List<char>();
+            listChar.Add('*');
+            listChar.Add('/');
+            listChar.Add('+');
+            listChar.Add('-');
+
+            List<Point> listPoint = new List<Point>();
+            listPoint.Add(p1);
+            listPoint.Add(p2);
+            listPoint.Add(new Point(5,6,listChar[3]));
+            listPoint.Add(new Point(7, 8, listChar[2]));
+
+            foreach(Point p in listPoint)
+            {
+                p.Draw();
+            }
+
+            Console.ReadLine();
         }
-        
+
     }
 }
