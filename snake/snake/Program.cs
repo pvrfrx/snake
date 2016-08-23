e273ecf4b69e25d5ae2fd436d08f2f89c8ee9837
pvrfrx@gmail.com <javarush1>
2016-08-23 13:43:24 +0500

    add DrawPoint

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,8 +10,22 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 5;
+            int y1 = 6;
+            char c1 = '*';
+            Draw(x1, y1, c1);
+
+            int x2 = 3;
+            int y2 = 2;
+            char c2 = '*';
+            Draw(x2, y2, c2);
+
             Console.ReadLine();
+        }
+        static void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
         }
     }
 }
